8ae739e2ce5d6ad4d8badcd96d476565a4d42736
WhiteSoul EvLF <144079178+WhiteSoulEvLF@users.noreply.github.com>
2023-10-07 12:13:22 +0530

    Update Requirements.sh

--- Requirements.sh
+++ Requirements.sh
@@ -1,3 +1,4 @@
+NC="\033[0m"
 clear
 apt update
 apt upgrade
@@ -8,9 +9,10 @@ apt install cowsay -y
 apt install nano -y
 apt install ruby -y
 gem install lolcat
+mkdir /data/data/com.termux/files/usr/themes
 clear
-figlet -f big Done !!! | lolcat
+figlet -f mono9 Done. | lolcat
 echo
-echo -e "[1m Now Run bash Main.sh"
+echo -e "[1m Now Run bash Main.sh${NC}"
 echo
-echo -e "[1m[32m Developed By :[33m WhiteSoul EvLF"
+echo -e "[1m[32m Developed By :[33m WhiteSoul EvLF${NC}"
